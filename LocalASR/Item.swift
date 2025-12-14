c527dca934a8f751f88e0ac03bf6ebb752a7b566
ixacik <patriklevak@gmail.com>
2025-12-14 10:07:38 +0100

    Initial Commit

---
+++ LocalASR/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  LocalASR
+//
+//  Created by Patres on 14/12/2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
